7b2676e2393cd05236a030e88ab98b938ff4956a
NemoGoldfarb <nemogoldfarb@gmail.com>
2024-01-30 19:19:51 +0100

    created project

--- VListProject/Program.cs
+++ VListProject/Program.cs
@@ -5,7 +5,23 @@ namespace VListServer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("boink");
+            Console.WriteLine("Hello World");
+        }
+    }
+    class Pokemon {
+        public readonly int id;
+        public readonly string name;
+        public bool wanted;
+        public void flip ()
+        {
+            if (wanted)
+            {
+                wanted=false;
+            }
+            else
+            {
+                wanted=true;
+            }
         }
     }
 }
